dc8c7ca660e695859a80dfa1f4322e6e744bf4e0
Abisha2309 <142960343+Abisha2309@users.noreply.github.com>
2023-09-16 11:56:32 +0530

    Add files via upload

---
+++ com.tnsif.programs/overloadingDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.overloading;
+
+public class overloadingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Points p=new Points();
+System.out.println(p);
+Points p1=new Points(1.5f);
+System.out.println(p1);
+Points p2=new Points(1.5f,2.5f);
+System.out.println(p2);
+
+	}
+
+}
