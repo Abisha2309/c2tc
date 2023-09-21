9905350f5b6ea8ce2e100fe0049f1eda64de2493
Abisha2309 <142960343+Abisha2309@users.noreply.github.com>
2023-09-21 10:55:32 +0530

    Add files via upload

---
+++ GenericEx.java
@@ -0,0 +1,18 @@
+package com.tns.Generics;
+
+import java.util.ArrayList;
+
+public class GenericEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+              ArrayList <String> l = new ArrayList <String>();
+              l.add("Hello");
+              for(String  str:l)
+              {
+            	  System.out.println(str);
+              }
+              
+	}
+
+}
